53eed69056e6a6db0a37f28f20b4e8cf4fbaefa7
adhilameenet <adhilameenet@gmail.com>
2022-08-26 15:48:18 +0530

    add:todo actions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,30 @@ import "./App.css";
 function App() {
   const [list, setList] = useState([]);
   const [todo, setTodo] = useState("");
+
+  const resetField = () => {
+    setTodo("");
+  }
+
+  const handleNullValues = (e) => {
+    e.preventDefault();
+    if(todo === "") {
+      window.alert("Text Field Must be Filled Out")
+    } else {
+      setList([...list, { value: todo, status: false, id: Date.now()}])
+      resetField();
+    }
+  }
+
+  const deleteTodo = (i) => {
+    var deletePermission = window.confirm("Do you want to delete the task ?");
+    if(deletePermission) {
+      const  todoTask = [...list];
+      todoTask.splice(i,1);
+      setList(todoTask);
+    }
+  }
+
   return (
     <div className="container">
       <div className="main-title">
@@ -18,9 +42,7 @@ function App() {
           placeholder="Add a New Task"
         />
         <i
-          onClick={() =>
-            setList([...list, { value: todo, status: false, id: Date.now() }])
-          }
+          onClick={handleNullValues}
           className="fas fa-plus"
         ></i>
       </div>
@@ -36,11 +58,11 @@ function App() {
                     }
                     return newData;
                   }))
-                }} value={data.status} type="checkbox" name="" id="" />
-                <p>{data.value}</p>
+                }} value={data.status} type="checkbox"/>
+                { data.status ? <strike><p>{data.value}</p></strike> : <p>{data.value}</p>}
               </div>
               <div className="right">
-                <i className="fas fa-times"></i>
+                <i onClick={deleteTodo} className="fas fa-times"></i>
               </div>
             </div>
           );
